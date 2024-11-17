923aabde69c2a06935e1535214df490e84e9f166
Nimira43 <n.rabone72@aol.com>
2024-11-17 12:01:43 +0000

    Finished the HTML for the Hero component.

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,6 +1,7 @@
 <script>
   // import smileyFace  from '@assets/smiley.png'
   import smiley from '../../assets/smiley.png'
+  import phoneVader from '../../assets/phone-vader.png'
 </script>
 
 <section class='hero'>
@@ -9,5 +10,12 @@
     <h1 class="dark mb-xs">Sign Up to Our Newsletter</h1>
     <h4 class="dark">News - Reviews - Tutorials - Code</h4>
     <img class="book-cover" src={smiley} alt="" width={200}/>
+    <img class="phone-cover" src={phoneVader} alt="" width={200}/>
+  </div>
+  <div class="hero-text white text-centre">
+    <h1>Find the Force</h1>
+    <h1>with Our Range </h1>
+    <h1 class='mb-m'> of Online Tutorials</h1>
+    <p>Unlock your coding potential with our expert-led online programming courses. Transform your career with hands-on training in the latest technologies. Enroll today and start coding your future!</p>
   </div>
 </section>
